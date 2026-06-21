d06b838912ea702e15f4a61f33d14f302d92cb3c
shupengqin <shupengqin@163.com>
2026-06-21 21:20:16 +0800

    Add mimic_iv_sql/00_create_schema.sql

---
+++ mimic_iv_sql/00_create_schema.sql
@@ -0,0 +1,6 @@
+-- Create study schema.
+-- Adjust schema name if your database permissions require a different namespace.
+
+DROP SCHEMA IF EXISTS saki_dynamic CASCADE;
+CREATE SCHEMA saki_dynamic;
+
